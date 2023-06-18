1341a3655b8db2fbad211ba4eeba41d21286641d
Hyeri <119188681+HyeriPark95@users.noreply.github.com>
2023-06-18 22:19:59 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,11 +1,17 @@
 class Solution {
-    public int solution(int num) {
-    long n = (long)num;
-    for(int i=0; i<500; i++){
-        if(n==1) return i; 
-      n = (n%2==0) ? n/2 : n*3+1;
-    }
-
-        return -1;
+    public int solution(long num) {
+        int answer = 0;
+        while(num!=1){
+            if(num%2==0){
+                num=num/2;
+            }else{
+                num=num*3+1;
+            }
+            answer++;
+        }
+        if(answer>500){
+            answer=-1;
+        }
+        return answer;
     }
 }
