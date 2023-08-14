03ace61fe051e3e0f0933c8e63fef14c3c140aac
박혜리 <119188681+HyeriPark95@users.noreply.github.com>
2023-08-15 00:36:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.17 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        //해시맵에 이름별로 선수 인원 넣기
         HashMap<String,Integer> startmem = new HashMap<String,Integer>();
         for(int i=0;i<participant.length;i++){
             String key = participant[i];
@@ -12,24 +13,22 @@ class Solution {
                 startmem.put(participant[i],1);
             }
         }
-        //
-
-        //
+        //완주한 멤버들 제거
         for(String end:completion){
             if(startmem.containsKey(end)){
                 int num = startmem.get(end)-1;
                 startmem.put(end,num);
             }
         }
-        //
+        //완주 못한 멤버 찾아서 answer에 넣어주고 loop탈출
         Iterator<String> keys = startmem.keySet().iterator();
-        loop:while(keys.hasNext()){
+        while(keys.hasNext()){
             String key = keys.next();
             if(startmem.get(key)>0){
                 answer = key;
-                break loop;
+                break;
             }
         }
-         return answer;
+        return answer;
     }
 }
