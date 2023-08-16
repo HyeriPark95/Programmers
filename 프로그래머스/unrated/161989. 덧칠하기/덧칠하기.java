f634b4b4d272ef8ad143d599e72d428d74dfdf29
박혜리 <119188681+HyeriPark95@users.noreply.github.com>
2023-08-17 00:26:52 +0900

    [unrated] Title: 덧칠하기, Time: 161.77 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer=0;
+        int[] roll = new int[n];
+        for(int a:section){
+            roll[a-1]=1;
+        }
+        for(int a:roll){
+            System.out.println(a);
+        }
+        
+        for(int i=0;i<roll.length;i++){
+            if(roll[i]==1){
+                answer++;
+                for(int j=0;j<m;j++){
+                    roll[Math.min(i+j,roll.length-1)]=0;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
