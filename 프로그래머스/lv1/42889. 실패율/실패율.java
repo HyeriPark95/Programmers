7d4152ee3bd9390f227007ae914f451b58ed3cc7
박혜리 <119188681+HyeriPark95@users.noreply.github.com>
2023-08-21 15:29:11 +0900

    [level 1] Title: 실패율, Time: 0.93 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -2,22 +2,21 @@ import java.util.*;
 class Solution {
     public ArrayList<Integer> solution(int N, int[] stages) {
         ArrayList<Integer> answer = new ArrayList<>();
-        double[] values= new double[N];
         Map<Integer,Double> fail = new HashMap<>();
         for(int i=1;i<=N;i++){
             int all=0;
             int inStage=0;
+            double fRate=0.0;
             for(int j=0;j<stages.length;j++){
                 if(stages[j]==i)inStage++;
                 if(stages[j]>=i)all++;
             }
-            if(all==0 && inStage==0){
-                fail.put(i,0.0);
-            }else{
-               fail.put(i,(double)inStage/all); 
+            if(!(all==0 && inStage==0)){
+               fRate = (double)inStage/all; 
             }
+            fail.put(i,fRate);
         }
-        List<Map.Entry<Integer,Double>> list = new LinkedList<>(fail.entrySet());
+        List<Map.Entry<Integer,Double>> list = new ArrayList<>(fail.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<Integer, Double>>() {
             @Override
             public int compare(Map.Entry<Integer, Double> o1, Map.Entry<Integer, Double> o2) {
@@ -27,7 +26,6 @@ class Solution {
         });
         for(Map.Entry<Integer,Double> a: list){
             answer.add(a.getKey());
-            System.out.println("key:"+a.getKey()+"value:"+a.getValue());
         }
         return answer;
     }
